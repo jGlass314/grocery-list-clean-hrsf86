669f9ec26842cd4f453769ab12636b725f1c5ed9
Josh Klein <jGlass314@gmail.com>
2017-12-30 18:08:15 -0800

    updates re-render part of the page, but more improvements possible

--- client/src/components/GroceryList.jsx
+++ client/src/components/GroceryList.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import GroceryItem from './GroceryItem.jsx';
+import AddGrocery from './AddGrocery.jsx';
+import $ from 'jquery';
 
 // const GroceryList = (props) => (
 class GroceryList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      list: this.props.list
     }
+  }
+
+  addGrocery(description, quantity) {
+    var tempList = this.state.list.slice();
+    var found = false;
+    tempList.forEach((item) => {
+      if(item.description.toLowerCase() === description.toLowerCase()) {
+        item.quantity = quantity;
+        found = true;
+      }
+    });
+    if(!found) {
+      tempList.push({id: tempList.length+1, quantity: quantity, description: description});
+    }
+    this.setState({
+      list: tempList
+    });
   }
 
   render() {
@@ -30,6 +49,7 @@ class GroceryList extends React.Component {
           }
         </tbody>
         </table>
+        <AddGrocery addGroceryFunction={this.addGrocery.bind(this)} />
       </div>
     )
   }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import List from '../../database/data.js';
 import GroceryList from './components/GroceryList.jsx';
-import AddGrocery from './components/AddGrocery.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,31 +17,12 @@ class App extends React.Component {
     });
   }
 
-  addGrocery(description, quantity) {
-    var tempList = this.state.list.slice();
-    var found = false;
-    tempList.forEach((item) => {
-      if(item.description.toLowerCase() === description.toLowerCase()) {
-        item.quantity = quantity;
-        found = true;
-      }
-    });
-    if(!found) {
-      tempList.push({id: tempList.length+1, quantity: quantity, description: description});
-    }
-    this.setState({
-      list: tempList
-    });
-  }
-
   render() {
     console.log('call to render on index.jsx');
     return (
       <div>
         <GroceryList list={this.state.list} />
-        <AddGrocery addGroceryFunction={this.addGrocery.bind(this)} />
       </div>
-
     )
   }
 }
